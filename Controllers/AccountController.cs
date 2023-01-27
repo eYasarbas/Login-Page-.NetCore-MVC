f2e2d10a35a33ac23ed64e4dfc734e4c04d6bd72
Enes Yaşarbaş <65676107+Huramiral2535@users.noreply.github.com>
2023-01-27 22:05:34 +0300

    Authorize and AllowAnonymous Atrribute and migration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MvcWebApp.Entities;
 using MvcWebApp.Models;
@@ -8,100 +10,173 @@ using NETCore.Encrypt.Extensions;
 
 namespace MvcWebApp.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
-        private readonly DatabaseContext _databseContext;
+        private readonly DatabaseContext _databaseContext;
         private readonly IConfiguration _configuration;
-        public AccountController(DatabaseContext databseContext, IConfiguration configuration)
+
+        public AccountController(DatabaseContext databaseContext, IConfiguration configuration)
         {
-            _databseContext = databseContext;
+            _databaseContext = databaseContext;
             _configuration = configuration;
         }
+
+        [AllowAnonymous]
         public IActionResult Login()
         {
             return View();
         }
+
+        [AllowAnonymous]
         [HttpPost]
         public IActionResult Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
-                string saltedPassword = model.Password + md5Salt;
-                string hashedPassword = saltedPassword.MD5();
+                string hashedPassword = DoMD5HashedString(model.Password);
 
-                User user = _databseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower()
-                && x.Password == hashedPassword);
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower() && x.Password == hashedPassword);
 
                 if (user != null)
                 {
                     if (user.Locked)
                     {
-                        ModelState.AddModelError(nameof(user.Username), "User is locked!");
+                        ModelState.AddModelError(nameof(model.Username), "User is locked.");
                         return View(model);
-
                     }
-                    List<Claim> claims = new List<Claim>();//for Cookie
-                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));//Global name for id
-                    claims.Add(new Claim(ClaimTypes.Name, user.FullName ?? string.Empty));//Global name for Fullname
-                    claims.Add(new Claim("Username", user.Username ?? string.Empty));
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name, user.FullName ?? string.Empty));
+                    claims.Add(new Claim(ClaimTypes.Role, user.Role));
+                    claims.Add(new Claim("Username", user.Username));
 
                     ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);//under code want to this
-                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal); //Login mvc
-                    return RedirectToAction("Index", "Home");//Go home
+
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Username or password is incorrect!");
+                    ModelState.AddModelError("", "Username or password is incorrect.");
                 }
             }
+
             return View(model);
         }
 
+        private string DoMD5HashedString(string s)
+        {
+            string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+            string salted = s + md5Salt;
+            string hashed = salted.MD5();
+            return hashed;
+        }
+
+        [AllowAnonymous]//enter without role        
         public IActionResult Register()
         {
             return View();
         }
 
-        public IActionResult Profile()
-        {
-            return View();
-        }
+        [AllowAnonymous]
         [HttpPost]
         public IActionResult Register(RegisterViewModel model)
         {
-
             if (ModelState.IsValid)
             {
-                if (_databseContext.Users.Any(x => x.Username.ToLower() == model.Username.ToLower()))
+                if (_databaseContext.Users.Any(x => x.Username.ToLower() == model.Username.ToLower()))
                 {
-                    ModelState.AddModelError(nameof(model.Username), "Username is already exist!");
+                    ModelState.AddModelError(nameof(model.Username), "Username is already exists.");
                     View(model);
                 }
-                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
-                string saltedPassword = model.Password + md5Salt;
-                //Since most of the passwords exist on the sites,
-                // we strengthen the incoming password.
-                string hashedPassword = saltedPassword.MD5();
+
+                string hashedPassword = DoMD5HashedString(model.Password);
+
                 User user = new()
                 {
                     Username = model.Username,
-                    Password = model.Password
-
+                    Password = hashedPassword
                 };
-                _databseContext.Users.Add(user);//new object add user of table
-                int affectedRowCount = _databseContext.SaveChanges();//insert
+
+                _databaseContext.Users.Add(user);
+                int affectedRowCount = _databaseContext.SaveChanges();
+
                 if (affectedRowCount == 0)
                 {
-                    ModelState.AddModelError("", "User can not be added!");
+                    ModelState.AddModelError("", "User can not be added.");
                 }
                 else
                 {
                     return RedirectToAction(nameof(Login));
                 }
             }
+
             return View(model);
         }
+
+        public IActionResult Profile()
+        {
+            ProfileInfoLoader();
+
+            return View();
+        }
+
+        private void ProfileInfoLoader()
+        {
+            Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+
+            ViewData["FullName"] = user.FullName;
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangeFullName([Required][StringLength(50)] string? fullname)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+
+                user.FullName = fullname;
+                _databaseContext.SaveChanges();
+
+                return RedirectToAction(nameof(Profile));
+            }
+
+            ProfileInfoLoader();
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangePassword([Required][MinLength(6)][MaxLength(16)] string? password)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+
+                string hashedPassword = DoMD5HashedString(password);
+
+                user.Password = hashedPassword;
+                _databaseContext.SaveChanges();
+
+                ViewData["result"] = "PasswordChanged";
+            }
+
+            ProfileInfoLoader();
+            return View("Profile");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Login));
+        }
+
     }
 }
